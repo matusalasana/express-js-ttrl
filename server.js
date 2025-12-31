64a1827ebf5bca4814cd6381e1b45c1e1878f780
Sana Matusala <matusalasana@gmail.com>
2025-12-31 13:34:57 +0300

    posts created, new post added, status code implemented

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
-const PORT = 3000;      // Define the port number
+dotenv.config(); // Load environment variables from .env file
+
+const PORT = process.env.PORT || 5000; // Define the port
 const path = require('path'); // Require path module
 const app = express();  // Create an Express application
 
 
-// setup static file serving
-app.use(express.static(path.join(__dirname, 'public')));
+let posts = [
+    {title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'},
+    {title: 'Post Three', body: 'This is post three'},
+];
+
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname , 'public', '/index.html'));
+// Get all posts
+app.get('/api/posts', (req, res) => {
+    res.status(200).json(posts);
 });
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname , 'public', '/about.html'));
+
+// Get a single post by ID
+app.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const post = posts.find(post => post.id === parseInt(id));
+    if (!post) {
+        return res
+            .status(404)
+            .json({ message: "Post not found" });
+    }
+    res.status(200).json(post);
+});
+
+// Create a new post
+app.post('/api/posts', express.json(), (req, res) => {
+    const newPost = {
+        id: posts.length + 1,
+        title: req.body.title,
+        body: req.body.body
+    };
+    posts.push(newPost);
+    res.status(201).json(newPost);
 });
 
 // Start the server and listen on the defined port
